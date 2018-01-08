81931158150171948ca3cff753777155c11a8286
0xADADA <0xadada.pub@0xadada.pub>
2018-01-08 07:07:04 -0500

    got tests passing

--- tests/unit/instance-initializers/application-test.js
+++ tests/unit/instance-initializers/application-test.js
@@ -1,33 +1,19 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import Application from '@ember/application';
-import { run } from '@ember/runloop';
-import initialize from 'dummy/instance-initializers/application';
+import { initialize } from 'ember-i18n-changeset-validations/instance-initializers/application';
 import applicationUtil from 'dummy/utils/application';
-import destroyApp from '../../helpers/destroy-app';
 
 module('Unit | Instance Initializer | application', function(hooks) {
   setupTest(hooks);
 
-  hooks.beforeEach(function() {
-    run(() => {
-      this.application = Application.create();
-      this.appInstance = this.application.buildInstance();
-    });
-  });
-
-  hooks.afterEach(function() {
-    run(this.appInstance, 'destroy');
-    destroyApp(this.application);
-  });
-
   test('initialize sets the app instance', function(assert) {
     assert.expect(1);
 
-    initialize(this.appInstance);
+    let stubAppInstance = 'asdf';
+    initialize(stubAppInstance);
 
     let result = applicationUtil.instance;
-    let expected = this.appInstance;
+    let expected = stubAppInstance;
     assert.deepEqual(result, expected, 'app instance is the appInstance');
   });
 });
