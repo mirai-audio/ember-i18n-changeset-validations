452dcc5bfb2f29ffe4bb5b97c955fe2becf4cb8e
0xADADA <0xadada.pub@0xadada.pub>
2018-01-10 14:27:57 -0500

    Added acceptance test

---
+++ tests/acceptance/application-test.js
@@ -0,0 +1,28 @@
+import { currentURL, findAll, visit } from 'ember-native-dom-helpers';
+import { test } from 'qunit';
+import moduleForAcceptance from 'dummy/tests/helpers/module-for-acceptance';
+
+const BETWEEN_ERROR_ES = 'Url debe estar entre 11 y 2048 caracteres';
+const VALID_ERROR_ES = 'Url debe ser una URL válida';
+
+moduleForAcceptance('Acceptance | application');
+
+test('changeset errors are translated on app/route/application', async function(assert) {
+  assert.expect(3);
+
+  await visit('/');
+
+  assert.equal(currentURL(), '/', 'application route loaded');
+
+  let [result_a, result_b] = await findAll('[data-test=url-error]');
+  assert.equal(
+    result_a.textContent.trim(),
+    BETWEEN_ERROR_ES,
+    'first title error is in Spanish'
+  );
+  assert.equal(
+    result_b.textContent.trim(),
+    VALID_ERROR_ES,
+    'second title error is in Spanish'
+  );
+});
